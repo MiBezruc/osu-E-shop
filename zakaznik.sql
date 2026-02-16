13500d1673ecfef00f4a4c2d4cbc89d68e5e1d9d
Mike Handsfree <Bezrmi91@osu.cz>
2026-02-16 11:26:20 +0100

    Přidal jsem sql databazi!

--- zakaznik.sql
+++ zakaznik.sql
@@ -34,7 +34,7 @@ CREATE TABLE `zakaznik` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexy pro exportované tabulky
+-- Indexy pro exportované tabulky jaj
 --
 
 --
